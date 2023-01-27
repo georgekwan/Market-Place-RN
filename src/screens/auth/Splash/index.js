32dee2e81a6ed06148509f29c149b4cac8a05c68
George <gwmkwan@gmail.com>
2023-01-27 08:49:15 -0700

    feat: create splash page

---
+++ src/screens/auth/Splash/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Splash = () => {
+  return (
+    <View>
+      <Text>You'll Find All You Need Here</Text>
+    </View>
+  );
+};
+
+export default Splash;

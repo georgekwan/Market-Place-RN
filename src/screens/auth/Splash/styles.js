a26d3661f66bfbbcfa5d7f5393a927df83b75b1e
George <gwmkwan@gmail.com>
2023-01-28 14:35:23 -0700

    style: add positioning to text container

--- src/screens/auth/Splash/styles.js
+++ src/screens/auth/Splash/styles.js
@@ -3,6 +3,12 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     padding: 24,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    flexGlow: 1,
+    height: '100%',
   },
   image: {
     width: '100%',

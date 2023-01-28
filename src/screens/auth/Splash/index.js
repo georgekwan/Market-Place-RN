e319614a3d4141b9bb69f2ba98e16f002a6459c9
George <gwmkwan@gmail.com>
2023-01-28 11:34:06 -0700

    fix: change splash image to correct directory

--- src/screens/auth/Splash/index.js
+++ src/screens/auth/Splash/index.js
@@ -1,9 +1,15 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { Image, Text, View } from 'react-native';
+import { styles } from './styles';
 
 const Splash = () => {
   return (
     <View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require('../../../assets/splash_image.png')}
+      />
       <Text>You'll Find All You Need Here</Text>
     </View>
   );

1e84fdc6cc354463f9b6cc7f3d07ad3eb9edbd8f
George <gwmkwan@gmail.com>
2023-01-28 14:07:03 -0700

    fix: correct styling location for button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,11 @@
 import { View, Text, Pressable, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { styles } from './styles';
 
 const Button = ({ title }) => {
   return (
     <View>
-      <Pressable>
+      <Pressable style={styles.container}>
         <Text>{title}</Text>
       </Pressable>
     </View>

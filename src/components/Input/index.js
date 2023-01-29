a795193d81fb5b60d4988cde70c09155fb30b93d
George <gwmkwan@gmail.com>
2023-01-29 10:46:47 -0700

    feat: create input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,16 @@
+import { View, Text, TextInput } from 'react-native';
+import React from 'react';
+import { styles } from './styles';
+
+const Input = ({ label }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.inputContainer} />
+      </View>
+    </View>
+  );
+};
+
+export default Input;

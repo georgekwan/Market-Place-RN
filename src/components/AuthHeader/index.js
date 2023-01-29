39b9e0373b4cb94f659872b7081c1cd1e14dd9fd
George <gwmkwan@gmail.com>
2023-01-28 18:02:58 -0700

    feat: create head component file

---
+++ src/components/AuthHeader/index.js
@@ -0,0 +1,19 @@
+import { Pressable, Text } from 'react-native';
+import React from 'react';
+import { styles } from './styles';
+
+const AuthHeader = ({ title, onBackPress }) => {
+  return (
+    <View style={styles.container}>
+      <Pressable hitStop={20} onPress={onBackPress}>
+        <Image
+          style={styles.image}
+          source={require('../../assets/auth_back.png')}
+        />
+      </Pressable>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+
+export default AuthHeader;

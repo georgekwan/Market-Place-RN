4ce27b6dec606910a2a1b2c5ec38deaa82f67ae9
George <gwmkwan@gmail.com>
2023-01-29 12:34:01 -0700

    feat: add show password when press on eye icon

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,17 +4,20 @@ import { styles } from './styles';
 import { endAsyncEvent } from 'react-native/Libraries/Performance/Systrace';
 
 const Input = ({ label, placeholder, isPassword }) => {
-  const [isPasswordVisable, setIsPasswordVisable] = useState(false);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const onEyePress = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{label}</Text>
       <View style={styles.inputContainer}>
         <TextInput
-          secureTextEntry={isPassword && !isPasswordVisable}
+          secureTextEntry={isPassword && !isPasswordVisible}
           placeholder={placeholder}
           style={styles.input}
         />
-        <Pressable>
+        <Pressable onPress={onEyePress}>
           <Image source={require('../../assets/eye.png')} />
         </Pressable>
       </View>

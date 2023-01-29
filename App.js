c3f34c815f296972beeafee9e54295fe0802c390
George <gwmkwan@gmail.com>
2023-01-28 18:12:24 -0700

    refactor: change function to es6

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import Splash from './src/screens/auth/Splash';
+import { SafeAreaView } from 'react-native';
+import Signup from './src/screens/auth/Signup';
 
-export default function App() {
+const App = () => {
   const [theme, setTheme] = useState('light');
   return (
     <SafeAreaView>
-      <Splash />
+      <Signup />
     </SafeAreaView>
   );
-}
+};
+
+export default App;

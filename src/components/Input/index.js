12fc077c341bcf09c7e98d77994d9660503930ad
George <gwmkwan@gmail.com>
2023-01-29 12:31:48 -0700

    feat: add eye icon to password field

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,17 +1,22 @@
-import { View, Text, TextInput } from 'react-native';
-import React from 'react';
+import { View, Image, Text, TextInput, Pressable } from 'react-native';
+import React, { useState } from 'react';
 import { styles } from './styles';
+import { endAsyncEvent } from 'react-native/Libraries/Performance/Systrace';
 
 const Input = ({ label, placeholder, isPassword }) => {
+  const [isPasswordVisable, setIsPasswordVisable] = useState(false);
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{label}</Text>
       <View style={styles.inputContainer}>
         <TextInput
-          secureTextEntry={isPassword}
+          secureTextEntry={isPassword && !isPasswordVisable}
           placeholder={placeholder}
           style={styles.input}
         />
+        <Pressable>
+          <Image source={require('../../assets/eye.png')} />
+        </Pressable>
       </View>
     </View>
   );

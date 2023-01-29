93d4e2fe1ea756b36d8c3eb1465dd33790e0c940
George <gwmkwan@gmail.com>
2023-01-28 18:03:44 -0700

    feat: add AuthHeader to sign up screen

--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
+import AuthHeader from '../../../components/AuthHeader';
 import { styles } from './styles';
 
 const Signup = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <AuthHeader />
+    </View>
+  );
 };
 
 export default Signup;

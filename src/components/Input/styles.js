a867e44958dc49c9d870b5b89c296b2d7f6c3bb7
George <gwmkwan@gmail.com>
2023-01-29 10:58:30 -0700

    feat: add label font style

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -3,7 +3,12 @@ import { colors } from '../../utils/colors';
 
 export const styles = StyleSheet.create({
   container: {},
-  label: {},
+  label: {
+    marginVertical: 8,
+    color: colors.blue,
+    fontSize: 14,
+    fontWeight: '500',
+  },
   inputContainer: {
     borderWidth: 1,
     borderColor: colors.grey,

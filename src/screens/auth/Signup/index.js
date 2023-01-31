cce848ffc46cfc1e7c50a351d98bb7d9f2a358cb
George <gwmkwan@gmail.com>
2023-01-31 08:30:07 -0700

    feat: add check box

--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import AuthHeader from '../../../components/AuthHeader';
+import Checkbox from '../../../components/Checkbox';
 import Input from '../../../components/Input';
 import { styles } from './styles';
 
 const Signup = () => {
+  const [checked, setChecked] = useState(false);
   return (
     <View style={styles.container}>
       <AuthHeader title="Sign Up" />
@@ -12,6 +14,9 @@ const Signup = () => {
       <Input label="Name" placeholder="John Doe" />
       <Input label="Email" placeholder="example@gmail.com" />
       <Input isPassword={true} label="Password" placeholder="********" />
+      <View>
+        <Checkbox checked={checked} onCheck={setChecked} />
+      </View>
     </View>
   );
 };

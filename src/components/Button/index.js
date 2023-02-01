cef66a21a00bf540f7d0ed631c1af244bd3d30e8
George <gwmkwan@gmail.com>
2023-02-01 08:09:10 -0700

    feat: pass style property as prop

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,12 +2,12 @@ import { Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { styles } from './styles';
 
-const Button = ({ title, onPress }) => {
+const Button = ({ title, onPress, style }) => {
   return (
     <TouchableOpacity
       activeOpacity={0.6}
       onPress={onPress}
-      style={styles.container}
+      style={[styles.container, style]}
     >
       <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>

93a97d2c37c3747ea4a8b17026bb2105258a035c
George <gwmkwan@gmail.com>
2023-02-04 11:22:38 -0700

    style: apply button styling

--- src/components/GoogleLogin/styles.js
+++ src/components/GoogleLogin/styles.js
@@ -5,8 +5,11 @@ export const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.darkGrey,
     borderRadius: 14,
-    width: '40%',
+    width: '45%',
     alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
   },
   image: {
     width: 30,

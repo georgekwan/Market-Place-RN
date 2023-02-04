eac492338ab2fb6c5e0929355ad499025ded18ac
George <gwmkwan@gmail.com>
2023-02-04 11:22:54 -0700

    fix: disable onPress

--- src/components/GoogleLogin/index.js
+++ src/components/GoogleLogin/index.js
@@ -1,4 +1,4 @@
-import { Image, Text, TouchableOpacity } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { styles } from './styles';
 
@@ -7,7 +7,7 @@ const GoogleLogin = () => {
   return (
     <TouchableOpacity
       activeOpacity={0.6}
-      onPress={onPress}
+      // onPress={onPress}
       style={styles.container}
     >
       <Image style={styles.image} source={require('../../assets/google.png')} />

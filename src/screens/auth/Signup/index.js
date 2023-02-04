42301bd384892a84e1fc1549c6503ca855df1ecf
George <gwmkwan@gmail.com>
2023-02-04 11:23:31 -0700

    feat: apply google login to sign up

--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -5,6 +5,7 @@ import Button from '../../../components/Button';
 import Checkbox from '../../../components/Checkbox';
 import Input from '../../../components/Input';
 import Separator from '../../../components/Separator';
+import GoogleLogin from '../../../components/GoogleLogin';
 import { styles } from './styles';
 
 const Signup = () => {
@@ -27,6 +28,8 @@ const Signup = () => {
       <Button style={styles.button} title="Sign Up" />
 
       <Separator text="Or sign up with " />
+
+      <GoogleLogin />
     </View>
   );
 };

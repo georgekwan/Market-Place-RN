6f3627570065e58d887eb08bcac171640abe98ff
George <gwmkwan@gmail.com>
2023-02-13 22:16:14 -0700

    feat: enable google sign in

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import Signup from './src/screens/auth/Signup';
-import Signin from './src/screens/auth/Signin';
-import Splash from './src/screens/auth/Splash';
 import { colors } from './src/utils/colors';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-// import { GoogleSignin } from '@react-native-google-signin/google-signin';
-// import Config from 'react-native-config';
+import Signup from './src/screens/auth/Signup';
+import Signin from './src/screens/auth/Signin';
+import Splash from './src/screens/auth/Splash';
+import Home from './src/screens/app/Home';
+import Favorites from './src/screens/app/Favorites';
+import Profile from './src/screens/app/Profile';
 
-// const WEB_CLIENT_ID =
-//   '356032109327-0eka9phlma63uoi7sgenobcgpum7i77n.apps.googleusercontent.com';
-// const IOS_CLIENT_ID =
-//   '356032109327-91jiecs7rjmuuupojnjkqspfbcfpq9c9.apps.googleusercontent.com';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import Config from 'react-native-config';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const Tabs = () => {
-  <Tab.Navigator>
+  <Tab.Navigator screenOptions={{ headerShown: false }}>
     <Tab.Screen name="Home" component={Home} />
     <Tab.Screen name="Favorites" component={Favorites} />
     <Tab.Screen name="Profile" component={Profile} />
@@ -28,14 +27,17 @@ const Tabs = () => {
 };
 
 const App = () => {
-  // useEffect(() => {
-  //   GoogleSignin.configure({
-  //     scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
-  //     webClientId: WEB_CLIENT_ID, // client ID of type WEB for your server (needed to verify user ID and offline access)
-  //     offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-  //     iosClientId: IOS_CLIENT_ID, // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
-  //   });
-  // }, []);
+  const isSignedIn = true;
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+      webClientId: WEB_CLIENT_ID, // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      accountName: '', // [Android] specifies an account name on the device that should be used
+      // iosClientId: IOS_CLIENT_ID,
+      // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    });
+  }, []);
 
   const theme = {
     colors: {

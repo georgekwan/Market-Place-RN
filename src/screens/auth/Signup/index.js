03e29aa85be94d8d1f13e9989778c284460e32a8
George <gwmkwan@gmail.com>
2023-02-13 21:05:31 -0700

    feat: apply SafeAreaView

--- src/screens/auth/Signup/index.js
+++ src/screens/auth/Signup/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import AuthHeader from '../../../components/AuthHeader';
 import Button from '../../../components/Button';
 import Checkbox from '../../../components/Checkbox';
@@ -20,35 +21,37 @@ const Signup = ({ navigation }) => {
   };
 
   return (
-    <ScrollView style={styles.container}>
-      <AuthHeader onBackPress={onBack} title="Sign Up" />
+    <SafeAreaView>
+      <ScrollView style={styles.container}>
+        <AuthHeader onBackPress={onBack} title="Sign Up" />
 
-      <Input label="Name" placeholder="John Doe" />
-      <Input label="E-mail" placeholder="example@gmail.com" />
-      <Input isPassword label="Password" placeholder="*******" />
+        <Input label="Name" placeholder="John Doe" />
+        <Input label="E-mail" placeholder="example@gmail.com" />
+        <Input isPassword label="Password" placeholder="*******" />
 
-      <View style={styles.agreeRow}>
-        <Checkbox checked={checked} onCheck={setChecked} />
-        <Text style={styles.agreeText}>
-          I agree with <Text style={styles.agreeTextBold}>Terms</Text> &{' '}
-          <Text style={styles.agreeTextBold}>Privacy</Text>
-        </Text>
-      </View>
+        <View style={styles.agreeRow}>
+          <Checkbox checked={checked} onCheck={setChecked} />
+          <Text style={styles.agreeText}>
+            I agree with <Text style={styles.agreeTextBold}>Terms</Text> &{' '}
+            <Text style={styles.agreeTextBold}>Privacy</Text>
+          </Text>
+        </View>
 
-      <Button style={styles.button} title="Sign Up" />
+        <Button style={styles.button} title="Sign Up" />
 
-      {/* <Separator text="Or sign up with" />
+        {/* <Separator text="Or sign up with" />
 
       <GoogleLogin /> */}
 
-      <Text style={styles.footerText}>
-        Already have an account?
-        <Text onPress={onSignIn} style={styles.footerLink}>
-          {' '}
-          Sign In
+        <Text style={styles.footerText}>
+          Already have an account?
+          <Text onPress={onSignIn} style={styles.footerLink}>
+            {' '}
+            Sign In
+          </Text>
         </Text>
-      </Text>
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 

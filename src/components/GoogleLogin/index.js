8488a0ad5fbe9f2c1cec94e22626364f52ea971e
George <gwmkwan@gmail.com>
2023-03-02 12:14:49 -0700

    feat: install react-native-logs

--- src/components/GoogleLogin/index.js
+++ src/components/GoogleLogin/index.js
@@ -7,6 +7,33 @@ import {
   statusCodes,
 } from '@react-native-google-signin/google-signin';
 
+import { logger, consoleTransport } from 'react-native-logs';
+
+const defaultConfig = {
+  levels: {
+    debug: 0,
+    info: 1,
+    warn: 2,
+    error: 3,
+  },
+  severity: 'debug',
+  transport: consoleTransport,
+  transportOptions: {
+    colors: {
+      info: 'blueBright',
+      warn: 'yellowBright',
+      error: 'redBright',
+    },
+  },
+  async: true,
+  dateFormat: 'time',
+  printLevel: true,
+  printDate: true,
+  enabled: true,
+};
+
+const log = logger.createLogger(defaultConfig);
+
 /*
 const GoogleLogin = () => {
   const handleGoogleLogin = async () => {
